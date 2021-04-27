64b516e90fa91b2844f663cbf635ff448651df7e
will <787010288@qq.com>
2021-04-27 22:13:58 +0800

    'test'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // let provider = 'http://114.115.173.246:38545'
 // let web3 = new Web3(new Web3.providers.HttpProvider(provider))
-let web3 = new Web3(window.ethereum)
+let provider = window.ethereum || window.BinanceChain
+console.log(window.BinanceChain, provider)
+let web3 = new Web3(provider)
 // web3.eth.getAccounts().then(res => {
 // 	console.log(res)
 // })
@@ -16,7 +18,7 @@ let web3 = new Web3(window.ethereum)
 // web3.eth.getChainId().then((res) => {
 // 	console.log(res)
 // });
-web3.eth.getNodeInfo().then(console.log)
+// web3.eth.getNodeInfo().then(console.log)
 // interest exchange electric razor liquid nuclear kingdom what dust occur sport piece
 function contract () {
 	var interface = [{
